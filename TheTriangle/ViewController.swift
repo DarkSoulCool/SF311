d13c91fd284951a7e41a560a11e3e07677c43c6c
user <user@PATA22.local>
2017-10-10 10:24:55 +0700

    Initial Commit

---
+++ TheTriangle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheTriangle
+//
+//  Created by user on 10/10/2560 BE.
+//  Copyright © 2560 Thammasat University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
